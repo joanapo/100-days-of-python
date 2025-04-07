2a8f2e39e77d29a140b124a6cd756ae35c62f750
joana <j.pocopkaite@gmail.com>
2025-04-07 12:58:22 +0100

    Day 26: dictionary comprehension

--- Learning and notes/Day 26/main.py
+++ Learning and notes/Day 26/main.py
@@ -1,3 +1,6 @@
+import random
+import pandas
+
 numbers = [1, 2, 3]
 new_numbers = [n + 1 for n in numbers]
 
@@ -9,3 +12,17 @@ doubled_list = [number * 2 for number in range(1, 5)]
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 long_names = [name.upper() for name in names if len(name) > 5]
 print(long_names)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+students_scores = {student: random.randint(1, 100) for student in names}
+passed_students = {student:score for (student, score) in students_scores.items() if score >= 40}
+
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_data_frame = pandas.DataFrame(student_dict)
+for (index, row) in student_data_frame.iterrows():
+    print(row.student)
