2a10d2c5adcbea69544bef3e91fdfadcfe91d17f
joana <j.pocopkaite@gmail.com>
2025-04-11 10:03:33 +0100

    Day 30: Add exception handling to the Nato Alphabet project

--- Daily Projects/Day 26: NATO Alphabet Project/main.py
+++ Daily Projects/Day 26: NATO Alphabet Project/main.py
@@ -3,6 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
 
-word = input("Please type a word: ").upper()
-nato_word = [nato_dictionary[letter] for letter in word]
-print(nato_word)
+def nato_alphabet():
+    while True:
+        try:
+            word = input("Please type a word: ").upper()
+            nato_word = [nato_dictionary[letter] for letter in word]
+            return nato_word
+
+        except KeyError:
+            print("Sorry, only letters in the alphabet please.")
+
+print(nato_alphabet())
